add6dd271c3984f6c5d0dc0d622616ef8e4452ae
SharmaHimanshu2021 <95370956+SharmaHimanshu2021@users.noreply.github.com>
2024-07-22 15:31:41 +0530

    Remove duplicates from sorted array.cpp

---
+++ Remove duplicates from sorted array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    int j=1;
+    for(int i=1;i<nums.size();i++){
+        if(nums[i]!=nums[i-1]){
+            nums[j]=nums[i];
+            j++;
+        }
+    }
+    return j;
+    }
+};
+//second approach
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    set<int> s1(nums.begin(),nums.end());
+    nums=vector<int>(s1.begin(),s1.end());
+    return s1.size();
+    }
+};
+
+//third apporach
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        vector<int>v2;
+    for(auto i=nums.begin();i<nums.end();i++){
+        if(binary_search(i+1,nums.end(),*i)==false)
+        v2.push_back(*i);
+    }
+    nums=v2;
+    return nums.size();
+    }
+};
