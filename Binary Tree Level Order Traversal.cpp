c5a37abea4905568e9e443ce5a5aeeaa786a234d
SharmaHimanshu2021 <95370956+SharmaHimanshu2021@users.noreply.github.com>
2024-08-12 12:00:24 +0530

    Update Binary Tree Level Order Traversal.cpp

--- Binary Tree Level Order Traversal.cpp
+++ Binary Tree Level Order Traversal.cpp
@@ -6,18 +6,18 @@ public:
         queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
-            int s=q.size();
-            vector<int>v;
-            for(int i=0;i<s;i++){
+            int levelsize=q.size();
+            vector<int>current;
+            for(int i=0;i<levelsize;i++){
                 TreeNode *node=q.front();
                 q.pop();
                 if(node->left!=NULL)
                      q.push(node->left);
                 if(node->right!=NULL)
                      q.push(node->right);
-                v.push_back(node->val);
+                current.push_back(node->val);
             }
-            output.push_back(v);
+            output.push_back(current);
         }
         return output;
     }
