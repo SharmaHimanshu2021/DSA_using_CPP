68a580999b4ba303a363481b52b6763168602238
SharmaHimanshu2021 <95370956+SharmaHimanshu2021@users.noreply.github.com>
2024-08-13 13:37:32 +0530

    Update Kth largest Number BST.cpp

--- Kth largest Number BST.cpp
+++ Kth largest Number BST.cpp
@@ -26,18 +26,18 @@
 ************************************************************/
 
 
-void reverseInorder(TreeNode<int>* root, vector<int> &v) {
+void Inorder(TreeNode<int>* root, vector<int> &v) {
     if (root != NULL) {
-        reverseInorder(root->right, v);
+        Inorder(root->right, v);
         v.push_back(root->data);
-        reverseInorder(root->left, v);
+        Inorder(root->left, v);
     }
 }
 
 int KthLargestNumber(TreeNode<int>* root, int k) 
 {
     vector<int> v;
-    reverseInorder(root, v);
+    Inorder(root, v);
     if (k <= v.size()) {
         return v[k-1];
     } else {
